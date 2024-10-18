3b19ec8c106f0dc4ca12ce206b41c16997f81124
Prathmesh Dilip Borate <prathmeshborate01@gmail.com>
2024-10-18 19:01:02 +0000

    Tailwind CSS done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html",
+            "./home.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
